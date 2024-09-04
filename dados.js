d0dacaa07df9a0ee83701ef3a6b769146b44441e
Rodrigo-Cruz-Dev <rococz1987@gmail.com>
2024-09-04 17:23:34 -0300

    inclusão dos arquivos JS

---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+    {
+        titulo: "Item 1",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem magni at dolor quibusdam, qui voluptatem sint aspernatur architecto ab doloribus laudantium quia omnis veritatis consequuntur porro laborum temporibus doloremque quas!",
+        link: "https://www.google.com.br/"
+    },
+
+    {
+        titulo: "Item 2",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem magni at dolor quibusdam, qui voluptatem sint aspernatur architecto ab doloribus laudantium quia omnis veritatis consequuntur porro laborum temporibus doloremque quas!",
+        link: "https://www.google.com.br/"
+    },
+
+    {
+        titulo: "Item 3",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem magni at dolor quibusdam, qui voluptatem sint aspernatur architecto ab doloribus laudantium quia omnis veritatis consequuntur porro laborum temporibus doloremque quas!",
+        link: "https://www.google.com.br/"
+    }
+];
