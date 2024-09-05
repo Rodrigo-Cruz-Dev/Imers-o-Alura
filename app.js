bdf0f2ef5d0416e36d886deb0cc9456bb7de7f5c
Rodrigo-Cruz-Dev <rococz1987@gmail.com>
2024-09-05 18:07:12 -0300

    comentario nos arquivos js

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 function pesquisar() {
-let section = document.getElementById("resultados-pesquisa")
-
-let resultados = ""
-
-for (let dado of dados) {
-    resultados += `
-<div class="item-resultado">
-<h2>
-<a href="#" target="blank">${dado.titulo}</a>
-</h2>
-<p class = "descricao-meta">${dado.descricao}</p>
-<a href=${dado.link} target="_blank" >Clique aqui para mais informações</a>
-</div>
-`
-}
-
-section.innerHTML = resultados
-
-}
-
-
+    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+  
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+      // Constrói o HTML para cada item do resultado da pesquisa, incluindo título, descrição e link
+      resultados += `
+  <div class="item-resultado">
+    <h2>
+      <a href="#" target="blank">${dado.titulo}</a>
+    </h2>
+    <p class = "descricao-meta">${dado.descricao}</p>
+    <a href=${dado.link} target="_blank" >Clique aqui para mais informações</a>
+  </div>
+      `;
+    }
+  
+    // Atribui o HTML gerado para a seção de resultados
+    section.innerHTML = resultados;
+  }
--- dados.js
+++ dados.js
@@ -1,19 +1,26 @@
+// Declara um array chamado "dados" para armazenar objetos que representam os resultados da pesquisa
 let dados = [
+    // Primeiro objeto do array, representando o primeiro resultado da pesquisa
     {
-        titulo: "Item 1",
-        descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem magni at dolor quibusdam, qui voluptatem sint aspernatur architecto ab doloribus laudantium quia omnis veritatis consequuntur porro laborum temporibus doloremque quas!",
-        link: "https://www.google.com.br/"
+      // Título do resultado
+      titulo: "Item 1",
+      // Descrição detalhada do resultado
+      descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem magni at dolor quibusdam, qui voluptatem sint aspernatur architecto ab doloribus laudantium quia omnis veritatis consequuntur porro laborum temporibus doloremque quas!",
+      // Link para mais informações sobre o resultado
+      link: "https://www.google.com.br/"
     },
-
+  
+    // Segundo objeto do array, representando o segundo resultado da pesquisa
     {
-        titulo: "Item 2",
-        descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem magni at dolor quibusdam, qui voluptatem sint aspernatur architecto ab doloribus laudantium quia omnis veritatis consequuntur porro laborum temporibus doloremque quas!",
-        link: "https://www.google.com.br/"
+      titulo: "Item 2",
+      descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem magni at dolor quibusdam, qui voluptatum sint aspernatur architecto ab doloribus laudantium quia omnis veritatis consequuntur porro laborum temporibus doloremque quas!",
+      link: "https://www.google.com.br/"
     },
-
+  
+    // Terceiro objeto do array, representando o terceiro resultado da pesquisa
     {
-        titulo: "Item 3",
-        descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem magni at dolor quibusdam, qui voluptatem sint aspernatur architecto ab doloribus laudantium quia omnis veritatis consequuntur porro laborum temporibus doloremque quas!",
-        link: "https://www.google.com.br/"
+      titulo: "Item 3",
+      descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem magni at dolor quibusdam, qui voluptatum sint aspernatur architecto ab doloribus laudantium quia omnis veritatis consequuntur porro laborum temporibus doloremque quas!",
+      link: "https://www.google.com.br/"
     }
-];
+  ];
