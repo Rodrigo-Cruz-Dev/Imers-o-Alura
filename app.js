e62d4ead8bdd352aa9c48e363d1873674eec9b01
Rodrigo-Cruz-Dev <rococz1987@gmail.com>
2024-09-05 17:57:31 -0300

    Inserido codigo js para os dados do site

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log(dados);
+function pesquisar() {
+let section = document.getElementById("resultados-pesquisa")
+
+let resultados = ""
+
+for (let dado of dados) {
+    resultados += `
+<div class="item-resultado">
+<h2>
+<a href="#" target="blank">${dado.titulo}</a>
+</h2>
+<p class = "descricao-meta">${dado.descricao}</p>
+<a href=${dado.link} target="_blank" >Clique aqui para mais informações</a>
+</div>
+`
+}
+
+section.innerHTML = resultados
+
+}
+
+
