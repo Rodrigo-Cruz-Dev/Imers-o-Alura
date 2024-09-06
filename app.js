455a0d481da3f4ddc52a4b5bdd4d34d3fd2c08c2
Rodrigo-Cruz-Dev <rococz1987@gmail.com>
2024-09-06 15:17:48 -0300

    adicionada pesquisa

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 function pesquisar() {
     // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
     let section = document.getElementById("resultados-pesquisa");
-  
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    if (campoPesquisa == "") {
+      section.innerHTML = "Busca vazia"
+      return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
     // Inicializa uma string vazia para armazenar os resultados formatados
     let resultados = "";
-  
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
     // Itera sobre cada dado na lista de dados
     for (let dado of dados) {
-      // Constrói o HTML para cada item do resultado da pesquisa, incluindo título, descrição e link
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        //Cria um novo elemento
+
       resultados += `
   <div class="item-resultado">
     <h2>
@@ -18,7 +34,7 @@ function pesquisar() {
   </div>
       `;
     }
-  
+   if (!resultados) { resultados = "Nada foi encontrado"}
     // Atribui o HTML gerado para a seção de resultados
     section.innerHTML = resultados;
-  }
+  }}
--- dados.js
+++ dados.js
@@ -5,22 +5,25 @@ let dados = [
       // Título do resultado
       titulo: "Item 1",
       // Descrição detalhada do resultado
-      descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem magni at dolor quibusdam, qui voluptatem sint aspernatur architecto ab doloribus laudantium quia omnis veritatis consequuntur porro laborum temporibus doloremque quas!",
+      descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem magni at dolor quibusdam",
       // Link para mais informações sobre o resultado
-      link: "https://www.google.com.br/"
+      link: "https://www.google.com.br/",
+      tags: "teste3"
     },
   
     // Segundo objeto do array, representando o segundo resultado da pesquisa
     {
       titulo: "Item 2",
-      descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem magni at dolor quibusdam, qui voluptatum sint aspernatur architecto ab doloribus laudantium quia omnis veritatis consequuntur porro laborum temporibus doloremque quas!",
-      link: "https://www.google.com.br/"
+      descricao: "qui voluptatum sint aspernatur architecto ab doloribus laudantium quia omnis veritatis consequuntur",
+      link: "https://www.google.com.br/",
+      tags:"teste2"
     },
   
     // Terceiro objeto do array, representando o terceiro resultado da pesquisa
     {
       titulo: "Item 3",
-      descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem magni at dolor quibusdam, qui voluptatum sint aspernatur architecto ab doloribus laudantium quia omnis veritatis consequuntur porro laborum temporibus doloremque quas!",
-      link: "https://www.google.com.br/"
+      descricao: "porro laborum temporibus doloremque quas!",
+      link: "https://www.google.com.br/",
+      tags: "teste1"
     }
   ];
